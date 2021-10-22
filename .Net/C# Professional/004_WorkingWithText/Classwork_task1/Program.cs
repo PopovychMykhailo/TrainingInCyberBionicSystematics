87d48c3d9514fc9f00187afa28fade84be93ceb4
PopovychMykhailo <michael.popovich.kiev@gmail.com>
2021-10-22 19:38:09 +0300

    Done lesson 004_WorkingWithText in Professional folder
    I completed only 1 and 3 tasks.
    2 and 4 I will do later (maybe).

---
+++ .Net/C# Professional/004_WorkingWithText/Classwork_task1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Classwork_task1
+{
+    /* Technical task 
+    
+        Напишите консольное приложение, позволяющие пользователю зарегистрироваться под
+        «Логином», состоящем только из символов латинского алфавита, и пароля, состоящего из
+        цифр и символов.
+    */
+
+
+    class Program
+    {
+        static void Main()
+        {
+            string userLogin;
+            string userPassword;
+
+            //Entering a user login
+            do
+            {
+                Console.Write("Create login: ");
+                userLogin = Console.ReadLine();
+
+                // If login is only letters
+                if (Regex.IsMatch(userLogin, @"^[A-Za-z]{3,}$") == true)
+                    break;
+                else
+                    Console.WriteLine("Wrong login! Login should have only letters!\n");
+
+            } while (true);
+
+            //Entering a user password
+            do
+            {
+                Console.Write("Create pass:  ");
+                userPassword = Console.ReadLine();
+
+                // If the data are numbers and symbols (not letters)
+                if (Regex.IsMatch(userPassword, @"^[\x21-\x40|\x5b-\x60|\x7b-\x7e]{8,}$") == true)
+                    break;
+                else
+                    Console.WriteLine("Wrong password! Password should have 8 numbers and/or symbols (not letters)!\n");
+
+            } while (true);
+
+            Console.WriteLine();
+            Console.WriteLine("Data succesffuly enter!");
+            Console.WriteLine($"Login:    {userLogin}");
+            Console.WriteLine($"Password: {userPassword}");
+        }
+    }
+}
---
+++ .Net/C# Professional/004_WorkingWithText/Homework_task3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Homework_task3
+{
+    /* Technical task
+    
+        Напишите шуточную программу «Дешифратор», которая бы в текстовом файле могла бы
+        заменить все предлоги на слово «ГАВ!»
+    */
+
+
+    class Program
+    {
+        public static object MathColection { get; private set; }
+
+        static void Main()
+        {
+            string pathFile = "Text.txt";
+            StreamReader streamReader = new(new FileStream(pathFile, FileMode.OpenOrCreate, FileAccess.Read));
+            StringBuilder text = new(streamReader.ReadToEnd().ToLower());
+            streamReader.Close();
+
+            string pattern = @"the|can";
+            string substitute = "Gav";
+
+            MatchCollection matchCollection = Regex.Matches(text.ToString(), pattern);
+
+            foreach (Match match in matchCollection)
+            {
+                text.Remove(match.Index, match.Groups[0].Value.Length);
+                text.Insert(match.Index, substitute);
+            }
+
+            StreamWriter streamWriter = new(new FileStream(pathFile, FileMode.OpenOrCreate, FileAccess.Write));
+            streamWriter.Write(text);
+            streamWriter.Close();
+        }
+    }
+}
