1c8176b7581756c9b772af7b8026a98614d5f2a4
PopovychMykhailo <michael.popovich.kiev@gmail.com>
2021-10-29 17:55:22 +0300

    Done lesson 009_GarbageCollection in Professional folder

---
+++ .Net/C# Professional/009_GarbageCollection/Classwork_task1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classwork_task1
+{
+    /* Technical task
+    
+        Создайте свой класс, объекты которого будут занимать много места в памяти (например, в
+        коде класса будет присутствовать большой массив) и реализуйте для этого класса
+        формализованный шаблон очистки.
+    */
+
+
+
+    class MyClass : IDisposable
+    {
+        public int[] bigArray;
+        public List<int> bigList;
+        private bool disposed = false;
+
+
+        public MyClass()
+        {
+            const int countArray = 50000000;
+
+            bigArray = new int[countArray];
+            for (int i = 0; i < countArray; i++)
+                bigArray[i] = i;
+
+            bigList = new(10);
+            for (int i = 0; i < bigList.Capacity; i++)
+                bigList.Add(i);
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine($"Size the array: {bigArray.Length}");
+            Console.WriteLine($"Size the list:  {bigList.Count}");
+        }
+
+
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose()");
+
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            Console.WriteLine("Dispose(bool disposing)");
+
+            if (!disposed)
+            {
+                // Clear classes
+                if (disposing)
+                {
+                    bigList.Clear();
+                }
+
+                // Clear simpler object
+                bigArray = null;
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"GetTotalMemory start: {GC.GetTotalMemory(false) / 1024}KB");        // How many memory
+
+            MyClass myClass = new();
+            myClass.ShowData();
+
+            Console.WriteLine($"GetTotalMemory after create: {GC.GetTotalMemory(false) / 1024}KB"); // How many memory
+            Console.ReadKey();
+
+            myClass.Dispose();  // Clear the object
+            GC.Collect();       // Clear memory in controls heap
+
+            Console.WriteLine($"GetTotalMemory after clear: {GC.GetTotalMemory(false) / 1024}KB");  // How many memory
+        }
+    }
+}
---
+++ .Net/C# Professional/009_GarbageCollection/Homework_task2/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework_task2
+{
+    /* Technical task
+    
+        Создайте класс, который позволит выполнять мониторинг ресурсов, используемых программой.
+        Используйте его в целях наблюдения за работой программы, а именно: пользователь может
+        указать приемлемые уровни потребления ресурсов (памяти), а методы класса позволят выдать
+        предупреждение, когда количество реально используемых ресурсов приблизиться к
+        максимально допустимому уровню.
+    */
+
+
+    class MyClass : IDisposable
+    {
+        public List<int> bigList;
+        const int addSizeInOneStep = 1000;
+        private bool disposed = false;
+
+
+        public MyClass()
+        {
+
+            bigList = new(addSizeInOneStep);
+            for (int i = 0; i < bigList.Capacity; i++)
+                bigList.Add(i);
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine($"Size the list:  {bigList.Count}");
+        }
+
+        public void AddSize()
+        {
+            for (int i = 0; i < addSizeInOneStep; i++)
+            {
+                bigList.Add(bigList.Count + i);
+            }
+        }
+
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose()");
+
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            Console.WriteLine("Dispose(bool disposing)");
+
+            if (!disposed)
+            {
+                // Clear classes
+                if (disposing)
+                {
+                    bigList.Clear();
+                }
+
+                // Clear simpler object
+                //bigArray = null;
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new();
+
+            long maxSizeUsesMemory;  // KiloBytes
+            long currentUsingMemery; // KiloBytes
+            Console.Write("Enter the maximum possible memory usage (in KB): ");
+            maxSizeUsesMemory = Convert.ToInt32(Console.ReadLine());
+
+            while (true)
+            {
+                currentUsingMemery = GC.GetTotalMemory(false) / 1024;
+                Console.WriteLine($"Current usage memory:  {currentUsingMemery} KB. " +
+                    $"{((decimal)currentUsingMemery / maxSizeUsesMemory * 100):0.0}%"); // How many memory
+
+                // If the current memory usage is less than 80% of the maximum allowable
+                if (currentUsingMemery < (maxSizeUsesMemory * 0.8))
+                {
+                    Console.WriteLine($"Usage memory is OK. Press for next");
+                }
+                else
+                {
+                    Console.WriteLine("Attention, memory is used more than 80% of the maximum allowable! Pressure for the next");
+                }
+
+                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                    break;
+
+                myClass.AddSize();
+            }
+
+            myClass.Dispose();
+            GC.Collect();
+
+            currentUsingMemery = GC.GetTotalMemory(false) / 1024;
+            Console.WriteLine($"Current usage memory:  {currentUsingMemery} KB. " +
+                    $"{((decimal)currentUsingMemery / maxSizeUsesMemory * 100):0.0}%"); // How many memory
+        }
+    }
+}
